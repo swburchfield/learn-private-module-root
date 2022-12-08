733c098ad64414837985049ae716d89a1ebca5fa
Steven Burchfield <sburchfield@aag.com>
2022-12-08 17:57:07 -0600

    Adding terraform cloud to workspace

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+   cloud {
+	  organization 	  = "myaag"
+	  workspaces {
+		name 		      = "learn-private-module-root"
+	  }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
